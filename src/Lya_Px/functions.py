c9376838643dcbc80a92a6022335b5c0cdc0e3cc
sindhusatyavolu <sindhusatyavolu@gmail.com>
2025-03-06 17:58:23 +0100

    added some functions

---
+++ src/Lya_Px/functions.py
@@ -0,0 +1,72 @@
+import numpy as np
+
+def angular_separation(ra1, dec1, ra2, dec2): 
+    # Calculate the difference in right ascension
+    delta_ra = ra2 - ra1  # in radians
+    
+    # Apply the formula for angular separation
+    angular_distance = np.arccos(np.sin(dec1) * np.sin(dec2) +
+                                 np.cos(dec1) * np.cos(dec2) * np.cos(delta_ra))
+    
+    return angular_distance # in radians
+
+def get_skewers(wave_fft_grid,mask_fft_grid,file,verbose=False):
+    skewers=[]
+    for hdu in file[1:]:
+        if len(skewers)%100==0 and verbose:
+            print(len(skewers),'read')
+        # quasar meta data
+        z_qso=hdu.header['Z']
+        # angles in radians
+        RA=hdu.header['RA']
+        Dec=hdu.header['DEC']
+        skewer={'z_qso':z_qso, 'RA':RA, 'Dec':Dec}
+        # forest data
+        wave_data=10.0**(hdu.data['LOGLAM'])
+        delta_data=hdu.data['DELTA']
+        weight_data=hdu.data['WEIGHT']
+        # correct the weights so that they are constant in redshift (for the raw analysis only)
+        weight_data *= (wave_data/4500)**3.8
+        skewer['wave_data']=wave_data
+        skewer['delta_data']=delta_data
+        skewer['weight_data']=weight_data
+
+        # Map the observed spectrum to the FFT grid for this particular redshift bin    
+        j_min_data=round((wave_data[0]-wave_fft_grid[0])/pw_A)
+        j_max_data=round((wave_data[-1]-wave_fft_grid[0])/pw_A)
+        
+        # map the data deltas and weights into the FFT grid
+        delta_fft_grid=np.zeros(N_fft)
+        weight_fft_grid=np.zeros(N_fft)
+        
+        # figure out whether the spectrum is cut at low-z or at high-z
+        loz_cut=False
+        hiz_cut=False
+        if j_min_data < 0:
+            loz_cut=True
+            if j_max_data >=0:
+                delta_fft_grid[:j_max_data]=delta_data[-j_min_data+1:]
+                weight_fft_grid[:j_max_data]=weight_data[-j_min_data+1:]
+        if j_max_data >= N_fft:
+            hiz_cut=True
+            if j_min_data < N_fft:
+                delta_fft_grid[j_min_data:]=delta_data[:N_fft-j_max_data-1]
+                weight_fft_grid[j_min_data:]=weight_data[:N_fft-j_max_data-1]
+        if loz_cut==False and hiz_cut==False:
+            delta_fft_grid[j_min_data:j_max_data+1]=delta_data
+            weight_fft_grid[j_min_data:j_max_data+1]=weight_data
+    
+        # limit the data to the pixels within this redshift bin
+        weight_fft_grid *= mask_fft_grid
+        
+        # store relevant information
+        skewer['j_min_data']=j_min_data
+        skewer['j_max_data']=j_max_data
+        skewer['delta_fft_grid']=delta_fft_grid
+        skewer['weight_fft_grid']=weight_fft_grid
+        skewers.append(skewer)
+    return skewers
+
+
+def get_separations(wave_fft_grid,mask_fft_grid):
+    return 0
--- src/Lya_Px/main.py
+++ src/Lya_Px/main.py
@@ -2,15 +2,71 @@ import numpy as np
 from astropy.io import fits
 import matplotlib.pyplot as plt
 
-
 from config import *
+from functions import *
+from px_from_pix import *
+import sys
+
+# First define the official DESI wavelength grid (all wavelengths that we could possibly care about)
+wave_desi_N = 5000
+# I know for sure that there is a pixel at 3600A, so let's make sure we cover that one
+wave_desi_min = 3600-500*pw_A 
+wave_desi_max = wave_desi_min+wave_desi_N*pw_A
+print('{:.2f} < lambda < {:.2f} [A]'.format(wave_desi_min, wave_desi_max))
+print('{:.3f} < z < {:.3f}'.format(wave_desi_min/LAM_LYA-1, wave_desi_max/LAM_LYA-1))
+wave_desi=np.linspace(wave_desi_min,wave_desi_max,wave_desi_N+1)
+
+
+if len(sys.argv) != 4:
+    print("Usage: python main.py <redshift_bin> <redshift_bin_width> <healpix pixel>")
+    sys.exit(1)
+
+z_alpha = float(sys.argv[1]) # redshift bin center
+dz = float(sys.argv[2]) # redshift bin width
+
+# figure out the center of the bin and its edges, in observed wavelength
+lam_cen = LAM_LYA*(1+z_alpha)
+lam_min = LAM_LYA*(1+z_alpha-0.5*dz)
+lam_max = LAM_LYA*(1+z_alpha+0.5*dz)
+print(lam_min,lam_cen,lam_max)
+
+# Create FFT grid in observed wavelength 
+# the FFT grid will have a fixed length of pixels (N_fft)  
+k = np.fft.fftfreq(N_fft)*2*np.pi/pw_A
+
+# figure out the index of the global (desi) grid that is closer to the center of the redshift bin
+i_cen = round((lam_cen-wave_desi_min)/pw_A) 
+wave_fft_grid = wave_desi[i_cen-N_fft//2:i_cen+N_fft//2] 
+print(wave_fft_grid[0],'< lambda <',wave_fft_grid[-1])
+
+mask_fft_grid = np.ones(N_fft) # placeholder for the mask in the FFT grid
+# while we use i to refer to indices in the global (desi) grid, we use j to refer to the FFT grid of this redshift
+j_cen = round((lam_cen-wave_fft_grid[0])/pw_A) 
+# this should alway be N_fft/2
+print(j_cen, N_fft//2)
+# figure out the indices (in the FFT grid) that fall within the redshift bin (top hat binning)
+j_min = round((lam_min-wave_fft_grid[0])/pw_A)
+j_max = round((lam_max-wave_fft_grid[0])/pw_A)
+print(j_min,j_max)
+mask_fft_grid[:j_min]=0
+mask_fft_grid[j_max:]=0
 
 # Read inputs
-healpix = int(input("Enter the Healpix pixel number: "))
+
+healpix = int(sys.argv[3])
 deltas_path='/global/cfs/cdirs/desi/science/lya/mock_analysis/develop/ifae-ql/qq_desi_y3/v1.0.5/analysis-0/jura-124/raw_bao_unblinding/deltas_lya/Delta/'
 delta_file=deltas_path+'delta-%d.fits.gz'%(healpix)
 file = fits.open(delta_file)
 
+# get sightlines from the delta file that fall within the fft grid
+skewers = get_skewers(wave_fft_grid,mask_fft_grid,file)
 
+# compute separations
+separation_angles = get_separations(skewers)
+
+# compute the power spectrum for each separation
+get_px(skewers,separation_angles)
+
+# save the results
 
 
