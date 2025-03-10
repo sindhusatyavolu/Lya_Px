21f5e5c183be08c918d6b0c36dad3ad77b132612
sindhusatyavolu <sindhusatyavolu@gmail.com>
2025-03-10 16:48:50 +0100

    update

--- src/Lya_Px/main.py
+++ src/Lya_Px/main.py
@@ -65,8 +65,8 @@ file = fits.open(delta_file)
 skewers = get_skewers(wave_fft_grid,mask_fft_grid,file)
 
 # compute separations
-separation_angles = get_separations(skewers)
-separation_angles,skewer_pairs = np.array(separation_angles)
+separation_angles,skewer_pairs = get_separations(skewers)
+separation_angles = np.array(separation_angles)
 print('minimum and maximum separation in degrees:',separation_angles.min()*RAD_TO_DEG,separation_angles.max()*RAD_TO_DEG)
 print(separation_angles[(separation_angles*RAD_TO_ARCMIN>6) & (separation_angles*RAD_TO_ARCMIN<9)])
 print(skewer_pairs[(separation_angles*RAD_TO_ARCMIN>6) & (separation_angles*RAD_TO_ARCMIN<9)])
