b0fc24cbe77c84fa60c6244d3858c78a5c032407
sindhusatyavolu <sindhusatyavolu@gmail.com>
2025-03-11 19:35:54 +0100

    update

--- src/Lya_Px/main.py
+++ src/Lya_Px/main.py
@@ -106,6 +106,6 @@ if plot_px:
     plt.savefig('px-%d.png'%(healpix))
 
 # save the results
-out_path = str(argv[4])
+out_path = str(sys.argv[4])
 np.savez('../bin/px-%d-%.2f-%.2f-%0.1f-0.1%f.npz'%(healpix,z_alpha,dz,theta_min*RAD_TO_ARCMIN,theta_max*RAD_TO_ARCMIN),k=k[:N_fft//2],px=px_norm[:N_fft//2])
 
