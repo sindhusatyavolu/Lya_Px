a85581d5e0819e61ce68bcb22168c45c5fa8a1f8
sindhusatyavolu <sindhusatyavolu@gmail.com>
2025-03-11 23:52:45 +0100

    added binning to post-processing

--- src/Lya_Px/post-processing.py
+++ src/Lya_Px/post-processing.py
@@ -6,6 +6,33 @@ path = str(sys.argv[1])
 
 k_arr = np.load(path)['k'] # in 1/A
 px = np.load(path)['px'] # in A
+karr_v = np.load(path)['k_vel'] # in s/km
+
+# bin power spectrum
+pos_k = k_arr[:N_FFT//2]
+k_bins = np.linspace(pos_k.min(),pos_k.max(),20)
+
+k_centers = 0.5 * (k_bins[:-1] + k_bins[1:])  # Compute bin centers
+
+# Digitize k values to find bin indices
+bin_indices = np.digitize(k_arr, k_bins) - 1 
+
+# Take moving average of Pk in each k bin
+Pbins = np.zeros(len(k_bins))
+for n in range(len(k_bins)):
+    Pbins[n] = np.mean(norm_Px[0][bin_indices==n])
+
+# Plot the binned power spectrum
+plt.plot(k_arr, norm_Px[0], label='Unbinned')
+plt.plot(k_centers, Pbins, label='Binned')
+plt.xlabel('k [1/A]')
+plt.ylabel('P(k) [A]')
+plt.legend()
+plt.show()
+plt.savefig('binned_px.png')
+
+
+
 
 
 
