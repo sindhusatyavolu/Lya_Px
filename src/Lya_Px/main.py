9e1d19279a4d6ba3c44d81aaf7a09f3e34d44069
sindhusatyavolu <sindhusatyavolu@gmail.com>
2025-03-18 14:11:15 +0100

    update

--- src/Lya_Px/main.py
+++ src/Lya_Px/main.py
@@ -185,7 +185,7 @@ for i in range(len(theta_min_array)):
     #px[i,:] *= norm_factor
     px_sum  = get_px(skewers,theta_min_array[i],theta_max_array[i])[3]
     print(px_sum-px[i,:])
-    assert (px_sum == px[i,:]).all()
+    assert np.allclose(px_sum == px[i,:])
 
 if plot_px:
     for i in range(len(theta_min_array)):
