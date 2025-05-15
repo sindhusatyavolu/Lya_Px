added Gaussian skewer tests and fixed saving of weights to use mean, not variance. Added notebook for example analysis of Gaussian test case"

src/Lya_Px/functions.py
@@ -2,7 +2,7 @@ import numpy as np
 from Lya_Px.params import *
 from Lya_Px.auxiliary import angular_separation
 from astropy.io import fits
-import argparse
+# import argparse
 from collections import defaultdict
 import fitsio
 
@@ -91,7 +91,7 @@ def create_skewer_class():
             self.delta_fft_grid = delta_fft_grid # real space delta in FFT grid
             self.weight_fft_grid = weight_fft_grid # real space weight in FFT grid
             self.mask_fft_grid = mask_fft_grid # real space mask in FFT grid 
-            
+            self.pw_A = pw_A
             return None 
                                
     return Skewers
@@ -166,11 +166,51 @@ def avg_over_healpixels(results):
     px_avg = {}
     px_var = {}
     p1d_avg = {}
+    px_avg_weights = {}
     for key in px_all:
         stacked = np.stack(px_all[key])
         stacked_weights = np.stack(px_weights_all[key])
         px_avg[key] = np.mean(stacked, axis=0)
         px_var[key] = np.var(stacked, axis=0)
-        px_weights[key] = np.var(stacked_weights, axis=0)  # count non-zero elements
+        px_avg_weights[key] = np.mean(stacked_weights, axis=0)  # count non-zero elements
         p1d_avg[key[0]] = np.mean(np.stack(p1d_all[key[0]]), axis=0)
-    return k_arr, px_avg, px_var, px_weights, p1d_avg
+    return k_arr, px_avg, px_var, px_avg_weights, p1d_avg
+
+
+def calculate_estnorm(W, R, L):
+    '''
+    W (np.ndarray): vector length N, average FFT of the weights
+    R (np.ndarray): vector length N, resolution in Fourier space
+    L (float): length of the spectra (in physical units, e.g. Angstroms or Mpc)
+    Returns:
+    estnorm (np.ndarray): vector length N, to be multiplied by every P1D mode of the measurement
+    '''
+    R2 = R.real**2 + R.imag**2
+    denom = np.absolute(np.fft.ifft(np.fft.fft(W)* np.fft.fft(R2)))
+    estnorm = np.absolute(L/denom)
+    return estnorm
+
+def calculate_window_matrix(W, R, L):
+
+    '''
+    W (np.ndarray): average of (w1) conj(w2) where w1 and w2 are FFT of original weights per skewer
+    R (np.ndarray): vector length N, resolution in Fourier space
+    L (float): physical length of skewers (e.g., in Angstroms)
+    Returns:
+    window_matrix (np.ndarray): window matrix to be convolved with pure theory
+    estnorm (np.ndarray): vector length N, to be multiplied by every P1D mode of the measurement
+    '''
+    R2 = R.real**2 + R.imag**2
+    denom = np.absolute(np.fft.ifft(np.fft.fft(W)* np.fft.fft(R2)))
+    estnorm = np.absolute(L/denom)
+    N = estnorm.size
+    window_matrix = np.zeros((N,N))
+    for m in range(N):
+        for n in range(N):
+            window_matrix[m,n] = W[m-n]*R2[n] / denom[m]
+    return window_matrix, estnorm
+
+
+def masked_theory(window_matrix, model):
+    return np.matmul(window_matrix, model)
+    

src/Lya_Px/params.py
@@ -23,9 +23,11 @@ dz = redshifts[:,1] # redshift bin width
 theta_array = np.loadtxt(theta_file, skiprows=1)
 healpix_file = config.get("parameters", "healpix_file")
 healpixlist = np.atleast_1d(np.loadtxt(healpix_file, dtype=int))
+gauss_test = config.getboolean("parameters","gauss_test")
 
 n_healpix = config.getint("parameters", "n_healpix")
-#healpixlist = healpixlist[-1:]
+ncpus = config.getint("parameters", "ncpus")
+healpixlist = healpixlist[:n_healpix]
 
 # Load constants
 LAM_LYA = config.getfloat("constants", "LAM_LYA")

src/Lya_Px/px_from_pix.py
@@ -1,48 +1,68 @@
 import numpy as np
 from Lya_Px.params import *
 from Lya_Px.auxiliary import angular_separation
-
+if gauss_test:
+    from Lya_Px.skewer_maker import SkewerMaker
+    from Lya_Px.input_power import InputPower
 def get_px(all_skewers,theta_min,theta_max):
 
     px_ft = np.zeros(N_fft)
     # mean of w_m v_m^* (product of FFT of masks)
     # w_v_m = (w_m v_m^*).real (product of FFT mask, real part only)
-    w_v_m=np.zeros(N_fft) 
-    
+    w_v_m=np.zeros(N_fft)
+    if gauss_test:
+        # Generate fake input power spectrum
+        input_p=InputPower(P0=0.5,k0=0.01,kF=0.1,f_px=0.7)
+        # read the skewer details from the first one, assuming all are equal-sized
+        Npix = all_skewers[0].delta_fft_grid.size
+        L_wave = all_skewers[0].pw_A * Npix
+        # initiate the skewer maker
+        maker=SkewerMaker(N=Npix,L=L_wave,input_power=input_p, seed=5432)
     products = []
     products_weight = []
     for i in range(1,len(all_skewers)):
-            for j in range(i):
-                separation_angle = angular_separation(all_skewers[j].RA,all_skewers[j].Dec,all_skewers[i].RA,all_skewers[i].Dec)
+        for j in range(i):
+            separation_angle = angular_separation(all_skewers[j].RA,all_skewers[j].Dec,all_skewers[i].RA,all_skewers[i].Dec)
 
-                if separation_angle>theta_min and separation_angle<theta_max:   # strictly less than theta_max and strictly greater than theta_min?
-                    delta1 = all_skewers[i].delta_fft_grid
-                    delta2 = all_skewers[j].delta_fft_grid
-                    weight1 = all_skewers[i].weight_fft_grid
-                    weight2 = all_skewers[j].weight_fft_grid
+            if separation_angle>theta_min and separation_angle<theta_max:   # strictly less than theta_max and strictly greater than theta_min?
+                delta1 = all_skewers[i].delta_fft_grid
+                delta2 = all_skewers[j].delta_fft_grid
+                weight1 = all_skewers[i].weight_fft_grid
+                weight2 = all_skewers[j].weight_fft_grid
+
+
+                if gauss_test:
+                    # generate two correlated Gaussian skewers as a known test case, use these instead of real delta1 and delta2
+                    g_skewer_1,g_skewer_2=maker.make_skewer_pair()
+                    # weight1 = np.ones(all_skewers[i].weight_fft_grid.shape) # uncomment if you want equal weighting for all pixels
+                    # weight2 = weight1 # uncomment if you want equal weighting for all pixels
+                    weighted_delta1 = g_skewer_1 * weight1
+                    weighted_delta2 = g_skewer_2 * weight2
+                    
+                else:
                     weighted_delta1 = delta1*weight1
                     weighted_delta2 = delta2*weight2
 
-                    # FT of weighted delta
-                    weighted_delta1_ft = np.fft.fft(weighted_delta1)
-                    weighted_delta2_ft = np.fft.fft(weighted_delta2)
 
-                    # compute the products
-                    px_sum = (weighted_delta1_ft*np.conj(weighted_delta2_ft)).real    
-                    products.append(px_sum)
-                    px_ft += px_sum
-                    fft_weight1 = np.fft.fft(weight1)
-                    fft_weight2 = np.fft.fft(weight2)
-                    products_weight.append((fft_weight1*np.conjugate(fft_weight2)).real)   
-                    
+                # FT of weighted delta
+                weighted_delta1_ft = np.fft.fft(weighted_delta1)
+                weighted_delta2_ft = np.fft.fft(weighted_delta2)
+                # compute the products
+                px_sum = (weighted_delta1_ft*np.conj(weighted_delta2_ft)).real
+                
+                products.append(px_sum)
+                px_ft += px_sum
+                fft_weight1 = np.fft.fft(weight1)
+                fft_weight2 = np.fft.fft(weight2)
+                products_weight.append((fft_weight1*np.conjugate(fft_weight2)).real)
+                
     # compute the variance of the products
     px_var = np.var(products,axis=0)
     px_ave = np.mean(products,axis=0)
     #print('variance and mean computed',px_ave)
     w_v_m = np.mean(products_weight,axis=0)
     #print('mean of product of fft of weights computed',w_v_m)   
-    print(len(products),'Number of pairs')
+    # print(len(products),'Number of pairs')
     return px_ft/len(products), w_v_m, px_var,px_ave,len(products)
-
 
 